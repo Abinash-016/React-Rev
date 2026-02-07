fc52ec73aa550a3b880d08cbb3262d8af0b23275
Abinash Mohapatra <mabinash016@gmail.com>
2026-02-07 20:47:44 +0530

    Conditional Rendering

--- src/DAY-3/App.jsx
+++ src/DAY-3/App.jsx
@@ -1,12 +1,14 @@
 import Style from "./style_in_react/Hello";
 import Prop_validation from "./prop-validation/Prop_Validation";
+import ConditionalRendering from "./conditional_Rendering/App";
 
 function Day3() {
   return (
     <div>
       <h1>Day 3</h1>
         {/* <Prop_validation/> */}
-        <Style/>
+        {/* <Style/> */}
+        <ConditionalRendering/>
     </div>
   );
 }
---
+++ src/DAY-3/conditional_Rendering/App.jsx
@@ -0,0 +1,10 @@
+import Conditional from "./Conditional";
+
+function ConditionalRendering(){
+    return(
+        <div>
+            <Conditional/>
+        </div>
+    )
+}
+export default ConditionalRendering
